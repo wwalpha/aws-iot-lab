97f99869e76e7300272ddde47be00c96c46039e1
wwalpha <wwalpha@gmail.com>
2022-10-15 16:19:42 +0900

    test

--- backend/kinesis_stream_events/src/index.ts
+++ backend/kinesis_stream_events/src/index.ts
@@ -3,7 +3,6 @@ import { KinesisStreamEvent } from 'aws-lambda';
 import { Tables } from 'typings';
 
 const TABLE_NAME_DEVICE_STATUS = process.env.TABLE_NAME_DEVICE_STATUS as string;
-const client = new DynamoDB.DocumentClient();
 
 export const handler = async (event: KinesisStreamEvent): Promise<any> => {
   event.Records.map((item) => console.log(item.kinesis.data));
