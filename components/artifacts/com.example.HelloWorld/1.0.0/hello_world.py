5eb719212f851c5ec881a562ccae515e2fddd17f
wwalpha <wwalpha@gmail.com>
2022-12-02 14:58:52 +0900

    uu

--- components/artifacts/com.example.HelloWorld/1.0.0/hello_world.py
+++ components/artifacts/com.example.HelloWorld/1.0.0/hello_world.py
@@ -1,7 +1,7 @@
 import sys
 
 message = "Hello, %s!" % sys.argv[1]
-message += " Greetings from your first Greengrass component.444"
+message += " Greetings from your first Greengrass component.555"
 
 # Print the message to stdout, which Greengrass saves in a log file.
 print(message)
